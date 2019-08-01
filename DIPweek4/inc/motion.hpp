3711ddd542ea28df1f2f18de8a1d729e4d66d78b
D.H. Kim <dhej1dark@gmail.com>
2019-08-01 20:21:45 +0900

    split

--- DIPweek4/inc/motion.hpp
+++ DIPweek4/inc/motion.hpp
@@ -9,7 +9,16 @@ using obj_f = std::function<double( const cv::Mat,
                                     const double,
                                     const std::vector<double>& )>;
 
-std::vector<std::tuple<int, int, int, int, int, int, double>>
+using mv_t = std::tuple<int, int, int, int, int, int, double>;
+
+using bma_f = std::function<mv_t( cv::Mat,
+                                  cv::Mat,
+                                  const cv::Rect,
+                                  obj_f,
+                                  const std::vector<double>&,
+                                  const std::vector<int>& )>;
+
+std::vector<mv_t>
 ebma( cv::Mat ancher_img,
       cv::Mat tracked_img,
       const int block_size,
--- DIPweek4/src/motion.cpp
+++ DIPweek4/src/motion.cpp
@@ -51,6 +51,14 @@ isInside( const int nrow_anch,
         return false;
 }
 
+bool
+isInsideRect( cv::Rect img, cv::Rect rect )
+{
+    auto tmp = rect & img;
+    auto tmpa = tmp == rect;
+    return tmpa;
+}
+
 std::vector<std::tuple<int, int, int, int, int, int, double>>
 ebma( cv::Mat ancher_img,
       cv::Mat tracked_img,
@@ -78,38 +86,34 @@ ebma( cv::Mat ancher_img,
             const int j_size =
                 j_idx + block_size > ncol ? ncol - j_idx : block_size;
 
-            cv::Rect ancher_rect( cv::Point(j_idx,i_idx), cv::Size(j_size,i_size) );
-            cv::Mat ancher_cut = ancher_img( ancher_rect );
+            cv::Rect ancher_rect( cv::Point( j_idx, i_idx ),
+                                  cv::Size( j_size, i_size ) );
 
             std::vector<std::tuple<double, int, int>> valid_error;
-// cv::Mat ancher_img, cv::Mat tracked_img, const int block_size, const cv::Rect orig_pos
-// obj_f objective, const std::vector<double>& obj_args
-// bma_f matcher, const std::vector<int>& bma_args
-            for ( int x = -( (int)search_range ); x < search_range; x++ )
+            std::vector<int> bma_args( {search_range} );
+            // cv::Mat ancher_img, cv::Mat tracked_img, const int block_size,
+            // const cv::Rect orig_pos obj_f objective, const
+            // std::vector<double>& obj_args bma_f matcher, const
+            // std::vector<int>& bma_args
+            cv::Mat ancher_cut = ancher_img( ancher_rect );
+
+            for ( int y = -bma_args[0]; y < bma_args[0]; y++ )
             {
-                for ( int y = -( (int)search_range ); y < search_range; y++ )
+                for ( int x = -bma_args[0]; x < bma_args[0]; x++ )
                 {
-                    auto method =
-                        std::bind( objective, ancher_cut, _1, x, y, obj_args );
-                    const bool isinside =
-                        isInside( nrow, ncol, i_idx + x, j_idx + y,
-                                  ancher_cut.rows, ancher_cut.cols );
+                    cv::Rect tracked_rect = ancher_rect + cv::Point( x, y );
+                    if ( !isInsideRect(
+                             cv::Rect( cv::Point( 0, 0 ), ancher_img.size() ),
+                             tracked_rect ) )
+                        continue;
 
-                    std::optional<cv::Mat> tracked_cut =
-                        isinside ? std::make_optional( tracked_img(
-                                       cv::Range( i_idx + x, i_idx + i_size + x ),
-                                       cv::Range( j_idx + y, j_idx + j_size + y ) ) )
-                                 : std::nullopt;
-
-                    std::optional<double> error_local =
-                        conv_optional( tracked_cut, method );
-
-                    if ( error_local.has_value() )
-                        valid_error.push_back(
-                            std::make_tuple( error_local.value(), x, y ) );
+                    valid_error.push_back( std::make_tuple(
+                        objective( ancher_cut, tracked_img( tracked_rect ), y,
+                                   x, obj_args ),
+                        y, x ) );
                 }
             }
-// end of block match algorithm
+            // end of block match algorithm
             std::sort( valid_error.begin(), valid_error.end() );
             auto least_pos =
                 std::make_tuple( i_idx, i_size + i_idx, j_idx, j_size + j_idx,
@@ -122,4 +126,84 @@ ebma( cv::Mat ancher_img,
     }
 
     return motion_vec;
+}
+
+std::vector<std::tuple<int, int, int, int, int, int, double>>
+bma( cv::Mat ancher_img,
+     cv::Mat tracked_img,
+     const int block_size,
+     const int search_range,
+     obj_f objective,
+     const std::vector<double>& obj_args,
+     bma_f matcher,
+     const std::vector<int>& bma_args )
+{
+    const int nrow = ancher_img.rows;
+    const int ncol = ancher_img.cols;
+    const int nrow_block = 1 + ( ( nrow - 1 ) / block_size );
+    const int ncol_block = 1 + ( ( ncol - 1 ) / block_size );
+
+    std::vector<mv_t> motion_vec;
+
+    for ( int i = 0; i < nrow_block; i++ )
+    {
+        const int i_idx = i * block_size;
+        const int i_size =
+            i_idx + block_size > nrow ? nrow - i_idx : block_size;
+
+        for ( int j = 0; j < ncol_block; j++ )
+        {
+            const int j_idx = j * block_size;
+            const int j_size =
+                j_idx + block_size > ncol ? ncol - j_idx : block_size;
+
+            cv::Rect ancher_rect( cv::Point( j_idx, i_idx ),
+                                  cv::Size( j_size, i_size ) );
+
+            std::vector<int> bma_args( {search_range} );
+
+            mv_t least_pos = matcher( ancher_img, tracked_img, ancher_rect,
+                                     objective, obj_args, bma_args );
+
+            motion_vec.push_back( least_pos );
+        }
+    }
+
+    return motion_vec;
+}
+
+mv_t
+ebma_f( cv::Mat ancher_img,
+        cv::Mat tracked_img,
+        const cv::Rect ancher_rect,
+        obj_f objective,
+        const std::vector<double>& obj_args,
+        const std::vector<int>& bma_args )
+{
+    std::vector<std::tuple<double, int, int>> valid_error;
+    cv::Mat ancher_cut = ancher_img( ancher_rect );
+
+    for ( int y = -bma_args[0]; y < bma_args[0]; y++ )
+    {
+        for ( int x = -bma_args[0]; x < bma_args[0]; x++ )
+        {
+            cv::Rect tracked_rect = ancher_rect + cv::Point( x, y );
+            if ( !isInsideRect(
+                     cv::Rect( cv::Point( 0, 0 ), ancher_img.size() ),
+                     tracked_rect ) )
+                continue;
+
+            valid_error.push_back( std::make_tuple(
+                objective( ancher_cut, tracked_img( tracked_rect ), y, x,
+                           obj_args ),
+                y, x ) );
+        }
+    }
+    // end of block match algorithm
+    std::sort( valid_error.begin(), valid_error.end() );
+    return std::make_tuple( ancher_rect.tl.y, ancher_rect.br.y,
+                            ancher_rect.tl.x, ancher_rect.br.x,
+                            std::get<1>( valid_error[0] ),  // x
+                            std::get<2>( valid_error[0] ),  // y
+                            (double)std::get<0>( valid_error[0] ) );
 }
